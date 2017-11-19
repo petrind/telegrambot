9dad201e973e95595273d0896e5f2192bf174bae
Petrus F. Subekti <petr_ind@yahoo.com>
2017-11-19 18:12:14 +0700

    [update] adding files for bot of year progress

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+export default {
+    telegram: {
+      token: process.env.TELEGRAM_TOKEN || '473931720:AAFhYh6h1dJUlVC0GmY_UkyiGpOrb72CDK8',
+      externalUrl: process.env.CUSTOM_ENV_VARIABLE || 'https://yoursite.com',
+      port: process.env.PORT || 443,
+      host: '0.0.0.0'
+    }
+  };
+  
---
+++ src/handlers/command.js
@@ -0,0 +1,36 @@
+import Time from "../services/time";
+import Progress from "../services/progress";
+
+const time = new Time();
+const progress = new Progress();
+
+export default class Command {
+  constructor() {}
+
+  getGreeting(message, bot) {
+    bot.sendMessage(message.from, 'Hi, there! It is nice to see you here, ${message.user.firstName} !');
+  }
+
+  getProgress(message, bot) {
+    const yearPercents = time.getYearProgress();
+    const yearProgress = progress.makeProgressString(yearPercents);
+
+    const monthPercents = time.getMonthProgress();
+    const monthProgress = progress.makeProgressString(monthPercents);
+
+    const dayPercents = time.getDayProgress();
+    const dayProgress = progress.makeProgressString(dayPercents);
+
+    const text =
+      "Year:    " + yearProgress + " " + yearPercents + "%\n" +
+      "Month: " + monthProgress + " " + monthPercents + "%\n" +
+      "Day:     " + dayProgress + " " + dayPercents + "%\n";
+
+    bot.sendMessage(message.from, text);
+  }
+
+  getHelp(message, bot) {
+    bot.sendMessage(message.from, 'Call the /progress to see how much time you waste');
+  }
+
+}
